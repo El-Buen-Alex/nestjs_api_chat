7b517a12372933e66a913a0553d882cb7c86812b
Daniel-Pincay <86489085+El-Buen-Alex@users.noreply.github.com>
2023-02-17 17:37:26 -0500

    add user table

---
+++ src/users/user.entity.ts
@@ -0,0 +1,31 @@
+import { TimeStampEntity } from "src/shared/main.entity";
+import { BaseEntity, BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import * as bcrypt from 'bcrypt';
+
+@Entity('users')
+export class UserEntity extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({name: 'first_name', nullable: false})
+    firstName: string;
+
+    @Column({name: 'last_name', nullable: false})
+    lastName: string;
+
+    @Column({name: 'email', nullable: false, unique: true})
+    email: string;
+
+    @Column({name: 'password', nullable: false})
+    password: string;
+
+    @Column(()=>TimeStampEntity, {prefix: false})
+
+    @BeforeInsert()
+    async hashPassword() {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    async validatePassword(password: string): Promise<boolean> {
+        return bcrypt.compare(password, this.password);
+    }
+}
